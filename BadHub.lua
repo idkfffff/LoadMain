9d10b98a85ffc832a81fb28e4dbe4714113885c4
BadTime <96690318+idkfffff@users.noreply.github.com>
2022-08-05 17:42:12 +0700

    Update and rename README.md to BadHub.lua

---
+++ BadHub.lua
@@ -0,0 +1,18 @@
+if game.PlaceId == 6329844902 then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/idkfffff/LastPirates/main/LastScript.lua'))()
+elseif game.PlaceId == 537413528 then
+	_G.Key = "BadHub"
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/idkfffff/BuildAsus/main/BadHub.lua'))()
+elseif game.PlaceId == 4587545091 or game.PlaceId == 7258239416 then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/idkfffff/VietNamPiece-Fucker/main/BadHub.lua'))()
+elseif game.PlaceId == 9744597824 then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/idkfffff/TESTTESTTESTESTESTESTTEST/main/BadHub.lua'))()
+elseif game.PlaceId == 2278581161 then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/idkfffff/Unleashed-Piece/main/s.lua'))()
+elseif game.PlaceId == 8657732325 then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/idkfffff/World-Sail/main/.lua'))()
+else
+	setclipboard("https://discord.gg/NWSfFgYmP2")
+	wait()
+	game.Players.LocalPlayer:Kick("Not Support For That Game, Find New Script in discord badhub link copied to your clipboard")
+end
